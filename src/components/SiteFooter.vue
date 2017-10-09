f5778792d4f34baafc20137c6bca2afa8d34c018
gregm711 <gregm@wearevest.com>
2017-10-08 20:52:57 -0400

    inputs coming along;

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,11 +1,12 @@
-<style lang="scss">
-
-
-</style>
-
 <template>
+    <div class="footer-cont">
 	<div class="small-logo"></div>
-
+    <div class="footer-text-cont">
+        <p class="footer-text"> Terms & Conditions</p>
+        <p class="footer-text"> Privacy Policy</p>
+        <p class="footer-text">@ copyright 2017 Curator</p>
+    </div>
+</div>
 </template>
 
 <script>
@@ -20,3 +21,28 @@ export default {
 }
 
 </script>
+
+<style lang="scss">
+.footer-text-cont {
+    float: right;
+    margin-top: 2.5rem;
+    width: 455px;
+    height: 20px;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 14px;
+    font-weight: 300;
+    line-height: 1.43;
+    letter-spacing: 0.2px;
+    text-align: center;
+    // margin-bottom: 3rem;
+    color: #ffffff;
+    .footer-text {
+        margin-right: 1.5rem;
+        display: inline-block;
+
+    }
+
+
+}
+
+</style>
--- src/components/contact/Contact.vue
+++ src/components/contact/Contact.vue
@@ -1,4 +1,36 @@
-<style scoped>
+<template>
+<div class="contact-wrapper">
+    <div class="contact">
+        <p>Sign up to reserve your spot on the waitlist!</p>
+    </div>
+    <div class="email-input-wrapper">
+        <input type="text" class="email-input" v-model="email" name="email" placeholder="">
+        <label class="input-label">Enter your email</label>
+    </div>
+        <button class="reserve-button">
+            <div class="reserve-my-spot">
+                Reserve My Spot
+            </div>
+        </button>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'contact',
+    data() {
+        return {
+            email: ""
+
+
+        }
+    }
+}
+
+</script>
+
+<style lang="scss">
 
 .contact {
     margin-top: 50px;
@@ -11,40 +43,97 @@
     letter-spacing: 0.2px;
     text-align: center;
     color: #ffffff;
+
 }
 
-
-.reserve-button-wrapper {
-    margin-top: 21px;
-}
 
 .email-input-wrapper {
-    margin-top: 20px;
+    margin: auto;
+    margin-top: 2rem;
+    font-family: Nunito;
+    position: relative;
+    width: 32.8rem;
+    height: 4.5rem;
+
 }
-.email {
-    width: 328px;
-    height: 45px;
-    border: none;
-    border-radius: 2px;
-    background-color: #ffffff;
+
+.input-label {
+      font-size:18px;
+      font-weight:normal;
+      position:absolute;
+      pointer-events:none;
+      left:35rem;
+      top:10px;
+      transition:0.2s ease all; 
+      -moz-transition:0.2s ease all; 
+      -webkit-transition:0.2s ease all;
+      width: 118px;
+    height: 22px;
     font-family: Nunito;
     font-size: 16px;
     font-weight: 300;
     letter-spacing: 0.2px;
     text-align: left;
     color: #000000;
-    padding-left: 22px;
-    box-shadow: 0 10px 15px 0 rgba(31, 32, 34, 0.3), 0 0 5px 0 rgba(31, 32, 34, 0.15);
 }
 
 
+.email-input {
+    width: 100%;
+    height: 100%;
+}
+
+.input-label{
+    position: absolute;
+    left: 0px;
+}
+// .email-input {
+//     width: 32.8rem;
+//     height: 4.5rem;
+//     border: none;
+//     border-radius: 2px;
+//     background-color: #ffffff;
+    
+//     font-size: 16px;
+//     font-weight: 300;
+//     letter-spacing: 0.2px;
+//     text-align: left;
+//     color: #000000;
+//     padding-left: 22px;
+//     box-shadow: 0 10px 15px 0 rgba(31, 32, 34, 0.3), 0 0 5px 0 rgba(31, 32, 34, 0.15);
+//     &:focus{
+//         height: 75px;
+//         // border-radius: 2px;
+//         // background-color: #ffffff;
+//         // box-shadow: 0 30px 25px 0 rgba(39, 20, 112, 0.5), 0 0 5px 0 rgba(31, 32, 34, 0.15);
+//         // border: solid 1px #1ab640;width: 200rem;
+//     }
+// }
+
+
+
 .reserve-button {
+    margin-top: 2.1rem;
     border-style: none;
-    width: 350px;
-    height: 55px;
-    border-radius: 8px;
+    width: 35rem;
+    height: 5.5rem;
+    border-radius: .8rem;
     background-image: linear-gradient(to bottom, #1dba74, #1a4f8d);
     box-shadow: 0 20px 25px 0 rgba(39, 20, 112, 0.5), 0 0 5px 0 rgba(31, 32, 34, 0.15);
+    &:hover{
+        background-image: linear-gradient(rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.05)), linear-gradient(to bottom, #1dba74, #1a4f8d);
+        box-shadow: 0 30px 25px 0 rgba(39, 20, 112, 0.5), 0 0 5px 0 rgba(31, 32, 34, 0.15);
+    }
+    &:active + clicked{
+        background-image: linear-gradient(rgba(0, 0, 0, 0.08), rgba(0, 0, 0, 0.08)), linear-gradient(to bottom, #1dba74, #1a4f8d);
+        box-shadow: 0 6px 10px 0 rgba(39, 20, 112, 0.2), 0 0 5px 0 rgba(31, 32, 34, 0.15);
+    }
+    /*&:hover {
+        opacity: 0;
+        background-color: black;
+        background-image: linear-gradient(rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.05)), linear-gradient(to bottom, #1dba74, #1a4f8d);
+        box-shadow: 0 30px 25px 0 rgba(39, 20, 112, 0.5), 0 0 5px 0 rgba(31, 32, 34, 0.15);
+    }*/
 }
 
 .reserve-my-spot {
@@ -73,36 +162,3 @@
 }
 
 </style>
-
-<template>
-<div class="contact-wrapper">
-    <div class="contact">
-        <p>Sign up to reserve your spot on the waitlist!</p>
-    </div>
-    <div class="email-input-wrapper">
-        <input type="text" class="email" v-model="email" name="email" placeholder="Enter your email">
-    </div>
-    <div class="reserve-button-wrapper">
-        <button class="reserve-button">
-            <div class="reserve-my-spot">
-                Reserve My Spot
-            </div>
-        </button>
-    </div>
-</div>
-</template>
-
-<script>
-
-export default {
-    name: 'contact',
-    data() {
-        return {
-            email: ""
-
-
-        }
-    }
-}
-
-</script>
